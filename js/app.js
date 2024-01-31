23d05adb7e95ce77eec97c4fd44a07a0aa64c48e
Pedro <phbr10.12@gmail.com>
2024-01-30 22:06:10 -0300

    most features

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
+const searchPokemonInput = document.getElementById("search-pokemon")
+const searchPokemonButton = document.getElementById("lupa-img")
 const pokemonCardContainer = document.getElementById("card-container")
+const aside = document.getElementById("aside")
+const closeBtn = document.getElementById("close-icon")
+const asideInfo = document.getElementsByClassName("aside-info")
+
 
 const typeColors = {
   fire: '#FDDFDF',
@@ -19,13 +25,14 @@ const typeColors = {
 
 const mainTypes = Object.keys(typeColors)
 
-const pokemonCount = 10
+const pokemonCount = 1025 
 
 const getPokemons = async (id) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`
   const response = await fetch(url)
   const data = await response.json()
   createPokemonCard(data)
+  return data
 }
 
 const fetchPokemons = async () => {
@@ -39,8 +46,10 @@ const createPokemonCard = (poke) => {
   card.classList.add("pokemon-card")
   
   const pokemonImg = poke.sprites.front_default
-  
+
   const pokemonName = poke.forms[0].name
+
+  const pokemonId = poke.id
   
   const pokemonType = poke.types.map(type => type.type.name)
   
@@ -48,10 +57,12 @@ const createPokemonCard = (poke) => {
   
   const color = typeColors[type]
   
+  card.style.background = color
+  
   let pokemonInnerHtml = `
   <div class="pokemon-card">
   <img src="${pokemonImg}" alt="Pokemon Imagem" id="poke-img">
-  <p>N° ${poke.id}</p>
+  <p>N° ${pokemonId}</p>
   <h4>${pokemonName}</h4>
   <div class="type">
   <p>${pokemonType[0]}</p>
@@ -62,7 +73,7 @@ const createPokemonCard = (poke) => {
   if(pokemonType[1]){
     pokemonInnerHtml = `
     <img src="${pokemonImg}" alt="Pokemon Imagem" id="poke-img">
-    <p>N° ${poke.id}</p>
+    <p>N° ${pokemonId}</p>
     <h4>${pokemonName}</h4>
     <div class="type">
     <p>${pokemonType[0]}</p>
@@ -72,7 +83,7 @@ const createPokemonCard = (poke) => {
   } else {
     pokemonInnerHtml = `
     <img src="${pokemonImg}" alt="Pokemon Imagem" id="poke-img">
-    <p>N° ${poke.id}</p>
+    <p>N° ${pokemonId}</p>
     <h4>${pokemonName}</h4>
     <div class="type">
     <p>${pokemonType[0]}</p>
@@ -85,16 +96,74 @@ const createPokemonCard = (poke) => {
   
   pokemonCardContainer.appendChild(card)
 
+  const getPokemonsAside = async (pokemon) => {
+    const data = await getPokemons(pokemon)
+    return data()
+  }
+  
+  const createAside = (poke) => {
+    aside.classList.add("active")
+    getPokemonsAside(pokemonId)
+
+    const pokemonGif = poke["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"]
+
+    const asideContainer = document.createElement("div")
+    asideContainer.classList.add("aside-info")
+
+    pokemonInnerHtml = `
+    <img src="${pokemonGif}" alt="" id="poke-img">
+      <p id="pokemon-id">N° ${pokemonId}</p>
+      <h2 id="pokemon-name">${pokemonName}</h2>
+      <div class="type">
+        <p>${pokemonType[0]}</p>
+        <p>${pokemonType[1]}</p>
+        </div>
+        <h4 id="pokedex-entry">Pokemon Entry</h4>
+        <p id="pokemon-entry">A strange seed was planted on its back at birth. the plant sprouts and grows with this pokémon.</p>
+        <div class="height-and-Weight">
+          <div class="Height"><h4>Height</h4><p>0.7m</p></div>
+          <div class="weight"><h4>Weight</h4><p>6.9kg</p></div>
+        </div>
+        <h4>Abilities</h4>
+        <div class="Abilities-container">
+          <div class="abilities">
+            <p id="abilities">Overgrow</p>
+            <p id="abilities">Chlorophyll</p>
+          </div>
+        </div>
+        <div class="stats-title"><h4>Stats</h4>
+          <div class="stats">
+            <p>49</p>
+            <p>49</p>
+            <p>49</p>
+            <p>49</p>
+            <p>49</p>
+            <p>49</p>
+            <p>49</p>
+          </div>
+        </div>
+        </div>`
+
+    asideContainer.innerHTML = pokemonInnerHtml
+
+    aside.appendChild(asideContainer)
+    
+    closeBtn.addEventListener("click", () => {
+      aside.classList.remove("active")
+      asideContainer.innerHTML = ``
+    })
+  }
+
+  card.addEventListener("click", (e) => {
+     createAside(poke)
+    }
+  )
 
 }
 
-
 fetchPokemons()
 
-createPokemonCard()
-
-pokemonCardContainer.addEventListener("click", (e) => {
-  console.log(e.target)
+searchPokemonButton.addEventListener("click", () => {
+  const searchPokemonInputValue = searchPokemonInput.value
 })
-
 
