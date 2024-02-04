760571508fb1d9ab4ac4ae1fe8ea328d9f79874d
Pedro <phbr10.12@gmail.com>
2024-02-04 20:30:54 -0300

    center card container

--- js/app.js
+++ js/app.js
@@ -105,7 +105,6 @@ const getPokemonsAside = async (pokemon) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
   const response = await fetch(url)
   const data = await response.json()
-  createAside(data)
 }
 
 getPokemonsAside(2)
